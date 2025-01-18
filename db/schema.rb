ea41b5b7318a4ea4584106a148fb4cb0236f575c
Alfredo Ormeno <ormenoandres13@gmail.com>
2025-01-18 01:23:22 -0500

    Add creator association to events
    - Added a `creator` association to the `Event` model, linking it to the
    `User` model.
    - Updated the `Event` model to include `belongs_to :creator`, specifying
    the `foreign_key` and `class_name`.
    - Created a migration to add the `creator_id` column to the `events`
    table with a foreign key constraint referencing the `users` table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_17_060851) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_18_054142) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -18,6 +18,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_17_060851) do
     t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "creator_id"
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,4 +33,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_17_060851) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "events", "users", column: "creator_id"
 end
