a9538a062e39798600d9da92b6b5ef5a7aacebc2
Alfredo Ormeno <ormenoandres13@gmail.com>
2025-01-25 02:54:10 -0500

    Display user's attended events
    - Add a list of "attended_events" to the User's
    Show page.
    - Provide visibility into the events a user is
    attending.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   def show
     @user = current_user
+    @attended_events = @user.attended_events
     @events = @user.created_events
   end
 end
