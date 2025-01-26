9aff9a9f65756db5cc45af14c2d888f118f150ea
Alfredo Ormeno <ormenoandres13@gmail.com>
2025-01-26 03:02:09 -0500

    Add separation of past and upcoming events on index page
    - Implement `past` and `upcoming` class methods in Event model.
    - Update EventsController to fetch past and upcoming events separately.
    - Modify the index view to display past and upcoming events in distinct
    sections.

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,6 +4,14 @@ class Event < ApplicationRecord
   # Event has a creator 
   belongs_to :creator, class_name: "User", foreign_key: "creator_id"
 
+  def self.past
+    where("date < ?", Date.today).order(date: :desc)
+  end
+
+  def self.upcoming
+    where("date >= ?", Date.today).order(date: :desc)
+  end
+
   #Event can have many attendees
   has_many :event_attendances, foreign_key: :attended_event_id, dependent: :destroy
   has_many :attendees, through: :event_attendances, source: :attendee
